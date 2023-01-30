897dab241a7d2ab91bf1e724615656e0120da24e
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-01-30 23:09:55 +0300

    feat: add classes for services interactivity

---
+++ plants/scripts/services.js
@@ -0,0 +1,10 @@
+const buttonGardens = document.querySelector('.button-services_gardens');
+const buttonLawn = document.querySelector('.button-services_lawn');
+const buttonPlanting = document.querySelector('.button-services_planting');
+
+const projectsGardens = document.querySelector('.project-type_gardens');
+const projectsLawn = document.querySelector('.project-type_lawn');
+const projectsPlanting = document.querySelector('.project-type_planting');
+
+let focusedGroups = [];
+
