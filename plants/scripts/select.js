4907b16508e320e1dfff12ed0588567dd1509d9a
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-02-04 21:38:05 +0300

    feat: fill city info

--- plants/scripts/select.js
+++ plants/scripts/select.js
@@ -16,13 +16,50 @@ for (const option of options) {
 }
 
 function openOrCloseSelect() {
-    switchClass(select, 'city-select_opened');
+    select.classList.toggle('city-select_opened')
 }
 
 function selectOption(option) {
+    const cityHolder = document.querySelector('.city-parameters > [name="phone"]');
+    const phoneHolder = document.querySelector('.city-parameters > [name="city"]');
+    const addressHolder = document.querySelector('.city-parameters > [name="address"]');
+    
+    const cityData = {
+        'Canandaigua, NY': {
+            phone: '+1	585	393 0001',
+            address: '151 Charlotte Street',
+        },
+        'Yonkers, NY': {
+            phone: '+1	914	678 0003',
+            address: '511 Warburton Ave',
+        },
+        'Sherrill, NY': {
+            phone: '+1	315	908 0004',
+            address: '14 WEST Noyes BLVD',
+        },
+        'New York City': {
+            phone: '+1	212	456 0002',
+            address: '9 East 91st Street',
+        },
+    }
+
     const city = option.innerText;
     selectedCity.innerText = city;
     select.classList.add('city-select_selected');
+    [
+        [cityHolder, city], 
+        [phoneHolder, cityData[city].phone],
+        [addressHolder, cityData[city].address]
+    ].forEach(([holder, value]) => {
+        holder.innerText = value
+    })
+    document.querySelector('.city-info').classList.add('city-info_visible')
+    document.querySelector('.city-info .call-us-button')
+        .setAttribute('href', cityData[city].phone)
+    ;
 
-
+    // for mobile
+    document.querySelector('.section.contacts')
+        .classList.add('section-contacts_remove-background-picture')
+    ;
 }
